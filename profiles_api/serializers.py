d93f630a48ed1c0a1f14ba938df59be6618ac5f5
PC-Ngumoha <pcngumoha@gmail.com>
2024-03-06 03:48:11 +0100

    Update: Added Profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,41 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
     """Serializes the API response from the view"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes the UserProfile model"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user instance"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data.get('email'),
+            name=validated_data.get('name'),
+            password=validated_data.get('password'),
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle the updating of user content"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
